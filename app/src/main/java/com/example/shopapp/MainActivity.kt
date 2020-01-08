5b60c5d061d1ff54687f5080e4b9defde6ad86f2
MvexD <muwiewow@gmail.com>
2020-01-08 08:14:38 +0100

    x

--- app/src/main/java/com/example/shopapp/MainActivity.kt
+++ app/src/main/java/com/example/shopapp/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.shopapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.shopapp.Model.Product
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val products = arrayListOf<Product>()
+
+        for (i in 0..100) {
+            products.add(Product("xD", "", 21.37))
+        }
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager ( this@MainActivity )
+            adapter = ProductsAdapter(products)
+        }
+
     }
 }
+
---
+++ app/src/main/java/com/example/shopapp/Model/Product.kt
@@ -0,0 +1,7 @@
+package com.example.shopapp.Model
+
+data class Product (
+    val title: String,
+    val photoUrl: String,
+    val price: Double
+)
